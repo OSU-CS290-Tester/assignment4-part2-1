f4da83d05d5ed52c31ea6628e19e418b04187c37
w-thomas <thomasw@onid.oregonstate.edu>
2015-02-15 13:45:52 -0800

    Implemented Category filter. Movies are ordered alphabetically

--- interface.php
+++ interface.php
@@ -3,14 +3,15 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 //Connect to sql server
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "thomasw-db", "", "thomasw-db");
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "thomasw-db", "s824hShW4EKidis5", "thomasw-db");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else {
 	echo "Connection worked!<br>";
 }
 
-//Check if value was posted and sets bound variables values
+//Check if value was posted and sets bound variables values. After submitting data
+//The form rediects to this page with values passed via POST
 if (isset($_POST['title'])) {
 	//prepared statement to input values into database.
 	if (!($stmt = $mysqli->prepare("INSERT INTO store_inventory(name, category, length) VALUES (?,?,?)"))) {
@@ -31,6 +32,8 @@ if (isset($_POST['title'])) {
 	} 
 }
 
+//If the delete button is called it redirects to this page passing that ID via url
+//This row is then deleted from the database via prepared statement.
 if(isset($_GET['deleted'])) {
 	$deleteID = $_GET['deleted'];
 	if (!($stmt = $mysqli->prepare("DELETE FROM store_inventory WHERE id = ?"))) {
@@ -46,15 +49,41 @@ if(isset($_GET['deleted'])) {
 	} 
 }
 
+//If the check in/out button is pressed, it passes values via url.
+//These values are checked here and a prepared statement is executed to 
+//switch the values on the database.
+if(isset($_GET['rented'])) {
+	$rentalID = $_GET['rented'];
+	$status = $_GET['status'];
+	$newStatus = NULL;
+	if ($status == 1) {
+		$newStatus = 0;
+	} else {
+		$newStatus = 1;
+	}
+
+	if (!($stmt = $mysqli->prepare("UPDATE store_inventory SET rented = ? WHERE id = ?"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	if (!$stmt->bind_param("ii", $newStatus, $rentalID)) {
+    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+	if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+	} 
+}
+
+//If delete all button is pressed this action occurs.
 if(isset($_POST['delete_all'])) {
 	!$mysqli->query("DELETE FROM store_inventory");
 }
 
+//Form to add videos.
 echo "<h2>Add a video into inventory</h2>";
 echo "<form action = 'interface.php' method = 'post'>";
 echo "Title: <input name = 'title' type = 'text'><br>";
 echo "Cateory: <input name = 'category' type = 'text'><br>";
-echo "Lenth: <input name = 'length' type = 'text'><br>";
+echo "Length: <input name = 'length' type = 'text'><br>";
 echo "<input name = 'add_movie' type = 'submit' value = 'Add Movie'>";
 echo "</form>";
 ?>
@@ -69,36 +98,47 @@ echo "</form>";
 
 <?php
 $menu_cat = NULL;
-//This section creates a drop down menu to filter by category. 
+
+//This section creates a drop down menu to filter by category. Pulls all categories
+//And creates options in a while loop.
 if (!($stmt = $mysqli->prepare("SELECT category FROM store_inventory"))) {
    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
-
 if (!$stmt->execute()) {
   	echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 }
-
 if (!$stmt->bind_result($menu_cat)) {
     echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
 }
-
+echo "<form action = 'interface.php' method = 'GET'>";
+echo "<select name = 'filter'>";
+echo "<option value = All>All</option>";
+while ($stmt->fetch()) {
+	echo "<option value =" . $menu_cat . ">" . $menu_cat . "</option>";
+}
+echo "<input type = 'submit' value = 'Filter'>";
+echo "</form>";
 
 
 //This section resets prepared statement to fetch data from database and print to the table.
-$cat_filter = 'all';
-
+$cat_filter = 'All';
+//If user has passed a filter choice it sets variable here, otherwise defaults to all.
 if(isset($_GET['filter'])) {
 	$cat_filter = $_GET['filter'];
 } 
 
-
-
-if($cat_filter == 'all') {
-	if (!($stmt = $mysqli->prepare("SELECT id, name, category, length, rented FROM store_inventory"))) {
+if($cat_filter == 'All') {
+	if (!($stmt = $mysqli->prepare("SELECT id, name, category, length, rented FROM store_inventory ORDER BY name"))) {
     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 	}
 } else {
-
+	if (!($stmt = $mysqli->prepare("SELECT id, name, category, length, rented FROM store_inventory WHERE category = ? ORDER BY name"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	
+	if (!$stmt->bind_param("s", $cat_filter)) {
+    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
 }
 
 $out_id = NULL;
@@ -120,20 +160,20 @@ echo "<th>Title</th>";
 echo "<th>Category</th>";
 echo "<th>Length</th>";
 echo "<th>Rented</th>";
-echo "<th>Check in/out</th>";
+echo "<th>Status</th>";
 echo "<th>Delete</th>";
 while ($stmt->fetch()) {
 	if ($out_rented == 1) {
-		$out_rented = "Checked in";
+		$status = "Checked in";
 	} else {
-		$out_rented = "Checked out";
+		$status = "Checked out";
 	}
 echo "<tr>";
 echo "<td>" . $out_name . "</td>";
 echo "<td>" . $out_cateory . "</td>";
 echo "<td>" . $out_length . "</td>";
-echo "<td>" . $out_rented . "</td>";
-echo "<td><a href='interface.php?rented=$out_id'><button>Check in/out</button></a></td>";
+echo "<td>" . $status . "</td>";
+echo "<td><a href='interface.php?rented=$out_id&status=$out_rented'><button>Check in/out</button></a></td>";
 echo "<td><a href='interface.php?deleted=$out_id'><button>Delete</button></a></td>";
 echo "</tr>";
 }
